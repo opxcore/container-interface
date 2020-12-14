f12fe5df95ba0934e946f2a6c8abec44dc41f330
lozovoyv <lozovoyv@gmail.com>
2020-12-14 20:40:57 +0300

    Add exception interfaces

---
+++ src/NotFoundExceptionInterface.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the OpxCore.
+ *
+ * Copyright (c) Lozovoy Vyacheslav <opxcore@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OpxCore\Container\Interfaces;
+
+interface NotFoundExceptionInterface extends \Psr\Container\NotFoundExceptionInterface
+{
+
+}
